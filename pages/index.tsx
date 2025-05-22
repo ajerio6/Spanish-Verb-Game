9a781c41aadc23310e371af1f9104a3f909d5de8
ajerio6 <danielajerio@gmail.com>
2025-05-22 11:16:37 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,3 +3,6 @@ import SpanishVerbGame from "../components/SpanishVerbGame";
 export default function Home() {
   return <SpanishVerbGame />;
 }
+git add pages/index.tsx
+git commit -m "Add index.tsx homepage for SpanishVerbGame"
+git push
